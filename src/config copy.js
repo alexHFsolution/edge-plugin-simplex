295f50a4742846ae6ec9cd3f2f7549d1fbee04e8
AndreyYaryha <andrey.yaryha@hf-solution.com>
2022-04-25 12:04:25 +0300

    turned back

--- src/config copy.js
+++
@@ -1,9 +0,0 @@
-// @flow
-const SANDBOX = false
-
-export const edgeUrl = SANDBOX ? 'https://edge-sandbox.test-simplexcc.com' : 'https://edge-prod.simplex.com'
-
-export const simplexUrl = SANDBOX ? 'https://sandbox.test-simplexcc.com/payments/new' : 'https://checkout.simplexcc.com/payments/new'
-
-export const edgeLegacyBuyUrl = /* SANDBOX ? 'https://simplex-sandbox-api.edgesecure.co' :  */'https://simplex-api.edgesecure.co'
-export const edgeSimplexBuyUrl = /* SANDBOX ? 'https://sandbox.test-simplexcc.com/payments/new' :  */'https://checkout.simplexcc.com/payments/new'
